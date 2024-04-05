4fccd14670bb02c94b429c50a22926ecf2aa0b36
Alexander Zharinov <alexander.zharinov@hiro-microdatacenters.nl>
2024-04-05 17:47:36 +0200

    Record VCR cassettes for E2E tests

--- app/tests/test_client.py
+++ app/tests/test_client.py
@@ -15,16 +15,18 @@ def test__call_send_to_invalid_host__raises(settings: Settings) -> None:
     assert isinstance(e.value, ClientError)
 
 
-# E2E test
+@pytest.mark.vcr()
 def test__call_send_to_valid_endpoint__no_exception() -> None:
     settings = Settings()  # type: ignore[call-arg]
     send_to_metadata_service("{}", settings)
 
 
-# E2E test
+@pytest.mark.vcr()
 def test__call_send_to_404_endpoint__raises() -> None:
     settings = Settings()  # type: ignore[call-arg]
     settings.metadata_service_url = settings.metadata_service_url + "test"
+
     with pytest.raises(ClientError) as e:
         send_to_metadata_service("{}", settings)
+
     assert isinstance(e.value, ClientError)
