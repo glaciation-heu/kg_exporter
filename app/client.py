49dfe898cf847d797dc04efd3396bfc414a68cbe
Alexander Zharinov <alexander.zharinov@hiro-microdatacenters.nl>
2024-04-05 17:39:19 +0200

    Make pre-commit green

--- app/client.py
+++ app/client.py
@@ -8,13 +8,13 @@ class ClientError(Exception):
     pass
 
 
-def send_to_metadata_service(message: str, settings: Settings):
+def send_to_metadata_service(message: str, settings: Settings) -> None:
     """Send graph data to Metadata Service"""
     try:
         httpx.patch(
             settings.metadata_service_url,
             content=message,
-            headers=[('Content-Type', 'application/json')],
+            headers=[("Content-Type", "application/json")],
         ).raise_for_status()
     except HTTPError as e:
         raise ClientError from e
--- app/tests/test_client.py
+++ app/tests/test_client.py
@@ -1,32 +1,30 @@
 import pytest
 
-from app.client import send_to_metadata_service, ClientError
+from app.client import ClientError, send_to_metadata_service
 from app.settings import Settings
 
 
 @pytest.fixture()
 def settings() -> Settings:
-    return Settings(
-        metadata_service_url="http://example.fake/api"
-    )
+    return Settings(metadata_service_url="http://example.fake/api")
 
 
-def test__call_send_to_invalid_host__raises(
-    settings: Settings
-) -> None:
+def test__call_send_to_invalid_host__raises(settings: Settings) -> None:
     with pytest.raises(ClientError) as e:
         send_to_metadata_service("{}", settings)
+    assert isinstance(e.value, ClientError)
 
 
 # E2E test
 def test__call_send_to_valid_endpoint__no_exception() -> None:
-    settings = Settings()
+    settings = Settings()  # type: ignore[call-arg]
     send_to_metadata_service("{}", settings)
 
 
 # E2E test
 def test__call_send_to_404_endpoint__raises() -> None:
-    settings = Settings()
-    settings.metadata_service_url = settings.metadata_service_url +"test"
+    settings = Settings()  # type: ignore[call-arg]
+    settings.metadata_service_url = settings.metadata_service_url + "test"
     with pytest.raises(ClientError) as e:
         send_to_metadata_service("{}", settings)
+    assert isinstance(e.value, ClientError)
