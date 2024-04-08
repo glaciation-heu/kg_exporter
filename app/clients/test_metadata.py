7218444210e8fe48f831124eb111fece50734cf0
Alexander Zharinov <alexander.zharinov@hiro-microdatacenters.nl>
2024-04-08 11:46:26 +0200

    Simplify client settings

--- app/clients/test_metadata.py
+++ app/clients/test_metadata.py
@@ -4,12 +4,8 @@ from app.clients.metadata import ClientError, send_to_metadata_service
 from app.settings import Settings
 
 
-@pytest.fixture()
-def settings() -> Settings:
-    return Settings(metadata_service_url="http://example.fake/api")
-
-
-def test__call_send_to_invalid_host__raises(settings: Settings) -> None:
+def test__call_send_to_invalid_host__raises() -> None:
+    settings = Settings(metadata_service_url="http://example.fake/api")
     with pytest.raises(ClientError) as e:
         send_to_metadata_service("{}", settings)
     assert isinstance(e.value, ClientError)
@@ -17,14 +13,14 @@ def test__call_send_to_invalid_host__raises(settings: Settings) -> None:
 
 @pytest.mark.vcr()
 def test__call_send_to_valid_endpoint__no_exception() -> None:
-    settings = Settings()  # type: ignore[call-arg]
+    settings = Settings()
     send_to_metadata_service("{}", settings)
 
 
 @pytest.mark.vcr()
 def test__call_send_to_404_endpoint__raises() -> None:
-    settings = Settings()  # type: ignore[call-arg]
-    settings.metadata_service_url = settings.metadata_service_url + "test"
+    settings = Settings()
+    settings.metadata_service_url = settings.metadata_service_url + "fake_url"
 
     with pytest.raises(ClientError) as e:
         send_to_metadata_service("{}", settings)
--- app/settings.py
+++ app/settings.py
@@ -2,8 +2,5 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    metadata_service_url: str
+    metadata_service_url: str = "http://metadata.integration/api/v0/graph"
     metadata_service_demo_message_period_sec: int = 60
-
-    class Config:
-        env_file = ".env"
