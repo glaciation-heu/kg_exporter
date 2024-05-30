ad6ba00edb586012a137ee766780e5326578a466
ktatarnikov <kostyantyn.tatarnikov@gmail.com>
2024-05-30 10:57:34 +0200

    [configfix] metadata service

--- app/settings.py
+++ app/settings.py
@@ -2,5 +2,5 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    metadata_service_url: str = "http://metadata.integration/api/v0/graph"
+    metadata_service_url: str = "http://metadata-service/api/v0/graph"
     metadata_service_push_period_sec: int = 60
