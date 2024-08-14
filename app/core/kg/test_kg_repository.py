006f57ccee658b456a525b0bf58b7f5a07a83171
ktatarnikov <kostyantyn.tatarnikov@gmail.com>
2024-08-14 12:15:42 +0200

    HHT-1174: minor fixes

--- app/core/kg/test_kg_repository.py
+++ app/core/kg/test_kg_repository.py
@@ -5,8 +5,15 @@ from app.clients.metadata_service.mock_metadata_service_client import (
     MockMetadataServiceClient,
 )
 from app.core.kg.kg_repository import KGRepository
+from app.core.kg.resource_status_query import (
+    ResourceStatus,
+    ResourceStatusQuery,
+    ResourceType,
+)
 from app.core.test_graph_fixture import TestGraphFixture
 from app.core.types import KGSliceId
+from app.kg.iri import IRI
+from app.kg.literal import Literal
 
 
 class KGRepositoryTest(TestCase, TestGraphFixture):
@@ -21,16 +28,25 @@ class KGRepositoryTest(TestCase, TestGraphFixture):
         graphs = client.take_inserts(slice_id.get_host_port())
         self.assertEqual(expected, graphs[0])
 
-    # TODO test query
-    # def test_query(self) -> None:
-    #     client = MockMetadataServiceClient()
-    #     repository = KGRepository(client)
-    #     slice_id = KGSliceId("127.0.0.1", 80)
-    #     query_str = "sparql query"
-    #     result_parser = KGTupleParser()
+    def test_query(self) -> None:
+        client = MockMetadataServiceClient()
+        repository = KGRepository(client)
+        slice_id = KGSliceId("127.0.0.1", 80)
+        query = ResourceStatusQuery(ResourceType.POD)
 
-    #     client.mock_query(slice_id.get_host_port(), query_str, [])
+        client.mock_query(
+            slice_id.get_host_port(),
+            query.get_query(),
+            [
+                {
+                    "resource": IRI("prefix", "id"),
+                    "statusValue": Literal("running", Literal.TYPE_STRING),
+                }
+            ],
+        )
 
-    #     actual = asyncio.run(repository.query(slice_id, query_str, result_parser))
-    #     expected = InMemoryGraph()
-    #     self.assertEqual(expected, actual)
+        actual = asyncio.run(repository.query(slice_id, query))
+
+        self.assertEqual(
+            [ResourceStatus(IRI("prefix", "id"), "running", ResourceType.POD)], actual
+        )
