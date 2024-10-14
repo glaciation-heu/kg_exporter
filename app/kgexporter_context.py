97f20d59745b120d8dbefbcaa8d0dce77bb41fca
ktatarnikov <kostyantyn.tatarnikov@gmail.com>
2024-10-14 11:01:20 +0200

    HHT-1417: terminated pods support

--- app/kgexporter_context.py
+++ app/kgexporter_context.py
@@ -9,6 +9,8 @@ from app.clients.k8s.k8s_client import K8SClient
 from app.clients.metadata_service.metadata_service_client import MetadataServiceClient
 from app.core.async_queue import AsyncQueue
 from app.core.builder.kg_builder import KGBuilder
+from app.core.k8s_updates.k8s_update_pool import K8SUpdatePool
+from app.core.k8s_updates.k8s_update_pool_impl import K8SUpdatePoolImpl
 from app.core.kg.kg_repository import KGRepository
 from app.core.repository.metric_repository import MetricRepository
 from app.core.repository.metricstore_client import MetricStoreClient
@@ -21,6 +23,7 @@ from app.util.clock import Clock
 class KGExporterContext:
     builder: KGBuilder
     updater: KGUpdater
+    k8s_pool: K8SUpdatePool
     queue: AsyncQueue[DKGSlice]
     runner: asyncio.Runner
     terminated: asyncio.Event
@@ -39,6 +42,7 @@ class KGExporterContext:
         self.settings = settings
         kg_repository = KGRepository(metadata_client)
         influxdb_repository = MetricRepository(metric_store_client)
+        self.k8s_pool = K8SUpdatePoolImpl(k8s_client)
         self.terminated = asyncio.Event()
         self.queue = AsyncQueue[DKGSlice]()
         self.builder = KGBuilder(
@@ -48,6 +52,7 @@ class KGExporterContext:
             k8s_client,
             kg_repository,
             influxdb_repository,
+            self.k8s_pool,
             self.settings.builder,
         )
         self.updater = KGUpdater(self.terminated, self.queue, kg_repository)
@@ -61,6 +66,7 @@ class KGExporterContext:
         self.runner.run(self.run_tasks())
 
     async def run_tasks(self) -> None:
+        self.tasks.append(asyncio.create_task(self.k8s_pool.subscribe()))
         self.tasks.append(asyncio.create_task(self.builder.run()))
         self.tasks.append(asyncio.create_task(self.updater.run()))
         self.prometheus_server = await start_http_server(
@@ -70,6 +76,8 @@ class KGExporterContext:
     def stop(self) -> None:
         self.terminated.set()
         self.runner.run(self.prometheus_server.close())
+        for task in self.tasks:
+            task.cancel()
 
     def wait_for_termination(self) -> None:
         self.runner.run(self.terminated.wait())
